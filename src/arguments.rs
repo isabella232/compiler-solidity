4beccf55c4c3e39474144f9cd31d0c7e52418b61
Dmitry Borisenkov <dmitry.borisenkov89@gmail.com>
2021-02-26 22:01:31 +0200

    Move to structopt
    Replace clap with structopt and move command-line arguments parsing into
    a separate module.

---
+++ src/arguments.rs
@@ -0,0 +1,36 @@
+//!
+//! Solidity to SyncVM compiler arguments.
+//!
+
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+///
+/// Solidity to SyncVM compiler arguments.
+///
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "Solidity compiler for SyncVM")]
+pub struct Arguments {
+    /// Input file
+    #[structopt(parse(from_os_str))]
+    pub input: PathBuf,
+
+    /// Function to run
+    #[structopt(short, long, default_value = "")]
+    pub run: String,
+
+    /// Options to pass to solidity compiler
+    #[structopt(long = "Xsol", default_value = "")]
+    pub xsol: String,
+}
+
+impl Arguments {
+    ///
+    /// A shortcut constructor.
+    ///
+    pub fn new() -> Self {
+        Self::from_args()
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,27 @@
-#[macro_use]
-extern crate clap;
-
 mod lib;
+pub mod arguments;
 
 use crate::lib::*;
-use std::path::Path;
+use self::arguments::Arguments;
 
 fn main() {
-    let matches = clap_app! (sol2tvm =>
-        (version: "0.1")
-        (author: "MatterLabs")
-        (about: "Solidity to Zinc translator")
-        (@arg INPUT: +takes_value +required "Input file")
-        (@arg RUN: -r +takes_value "function to run")
-        (@arg XSOL: +takes_value "Command line options to pass to Solidity compiler")
-    )
-    .get_matches();
+    let args = Arguments::new();
 
-    let file_name = matches.value_of("INPUT").unwrap();
+    let file_name = args.input;
 
-    if !Path::new(file_name).exists() {
-        panic!("{} does not exist", file_name);
+    println!("Input {}", file_name.to_str().unwrap());
+    if !file_name.exists() {
+        panic!("{} does not exist", file_name.to_str().unwrap());
     }
 
-    let file_type = file_type(file_name);
-    println!("{:?}", file_type);
+    let opts = args.xsol.as_str();
 
-    let opts = matches.value_of("XSOL").unwrap_or("");
+    let run = match args.run.as_str() {
+        "" => None,
+        v => Some(v),
+    };
 
-    let run = matches.value_of("RUN");
-    let actions = generate_actions(file_name, opts, &run);
-    println!("{:?}", actions);
+    let actions = generate_actions(&file_name, opts, &run);
 
     for a in actions.iter() {
         execute_action(a);
