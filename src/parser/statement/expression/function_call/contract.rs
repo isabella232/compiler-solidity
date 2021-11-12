ee9e59b5e5334c3ef541af20f6bc4a2d992b2345
Alex Z <a.zarudnyy@matterlabs.dev>
2021-11-12 23:11:34 +0200

    Add the temporary external call selector UMA optimization

--- src/parser/statement/expression/function_call/contract.rs
+++ src/parser/statement/expression/function_call/contract.rs
@@ -23,12 +23,10 @@ pub fn call<'ctx, 'src>(
     let intrinsic = context.get_intrinsic_function(Intrinsic::SwitchContext);
     context.build_call(intrinsic, &[], "contract_call_switch_context");
 
-    let child_pointer_input = context.access_memory(
-        context.field_const(
-            (compiler_common::abi::OFFSET_CALLDATA_SIZE * compiler_common::size::FIELD) as u64,
-        ),
-        compiler_common::AddressSpace::Child,
-        "contract_call_child_pointer_input",
+    let input_offset_adjusted = context.builder.build_int_add(
+        input_offset,
+        context.field_const(4),
+        "contract_call_input_offset_adjusted",
     );
     let input_size_without_selector = context.builder.build_int_sub(
         input_size,
@@ -38,6 +36,32 @@ pub fn call<'ctx, 'src>(
     let input_size_adjusted = context.ceil32(
         input_size_without_selector,
         "contract_call_input_size_adjusted",
+    );
+
+    let input_selector_pointer = context.access_memory(
+        input_offset,
+        compiler_common::AddressSpace::Heap,
+        "contract_call_input_selector_pointer",
+    );
+    let input_selector = context.build_load(input_selector_pointer, "input_selector");
+    let input_selector_masked = context.builder.build_and(
+        input_selector.into_int_value(),
+        context
+            .field_type()
+            .const_int_from_string(
+                "ffffffff00000000000000000000000000000000000000000000000000000000",
+                inkwell::types::StringRadix::Hexadecimal,
+            )
+            .expect("Always exists"),
+        "contract_call_input_selector_masked",
+    );
+
+    let child_pointer_input = context.access_memory(
+        context.field_const(
+            (compiler_common::abi::OFFSET_CALLDATA_SIZE * compiler_common::size::FIELD) as u64,
+        ),
+        compiler_common::AddressSpace::Child,
+        "contract_call_child_pointer_input",
     );
     context.build_store(
         child_pointer_input,
@@ -62,17 +86,24 @@ pub fn call<'ctx, 'src>(
             "contract_call_output_size_cells",
         ),
     );
+    let child_pointer_selector = context.access_memory(
+        context.field_const(
+            (compiler_common::abi::OFFSET_ENTRY_DATA * compiler_common::size::FIELD) as u64,
+        ),
+        compiler_common::AddressSpace::Child,
+        "contract_call_child_pointer_selector",
+    );
+    context.build_store(child_pointer_selector, input_selector_masked);
 
     let destination = context.access_memory(
         context.field_const(
-            (compiler_common::abi::OFFSET_CALL_RETURN_DATA * compiler_common::size::FIELD - 4)
-                as u64,
+            (compiler_common::abi::OFFSET_CALL_RETURN_DATA * compiler_common::size::FIELD) as u64,
         ),
         compiler_common::AddressSpace::Child,
         "contract_call_child_input_destination",
     );
     let source = context.access_memory(
-        input_offset,
+        input_offset_adjusted,
         compiler_common::AddressSpace::Heap,
         "contract_call_child_input_source",
     );
@@ -81,7 +112,7 @@ pub fn call<'ctx, 'src>(
         Intrinsic::MemoryCopyToChild,
         destination,
         source,
-        input_size,
+        input_size_without_selector,
         "contract_call_memcpy_to_child",
     );
 
