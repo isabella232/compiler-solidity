751e7e4441389a1307406cce720edf57498e1399
Alex Z <a.zarudnyy@matterlabs.dev>
2021-11-17 15:19:56 +0200

    Simplify calldatacopy

--- src/parser/statement/expression/function_call/calldata.rs
+++ src/parser/statement/expression/function_call/calldata.rs
@@ -54,41 +54,6 @@ pub fn copy<'ctx, 'src>(
     context: &mut LLVMContext<'ctx, 'src>,
     arguments: [inkwell::values::BasicValueEnum<'ctx>; 3],
 ) -> Option<inkwell::values::BasicValueEnum<'ctx>> {
-    let copy_block = context.append_basic_block("calldata_if_copy");
-    let zero_block = context.append_basic_block("calldata_if_zero");
-    let join_block = context.append_basic_block("calldata_if_join");
-
-    let pointer = context.access_memory(
-        context.field_const(
-            (compiler_common::abi::OFFSET_CALLDATA_SIZE * compiler_common::size::FIELD) as u64,
-        ),
-        compiler_common::AddressSpace::Parent,
-        "calldata_size_pointer",
-    );
-    let calldata_size = context
-        .build_load(pointer, "calldata_size_value_cells")
-        .into_int_value();
-
-    let range_end_bytes = context.builder.build_int_add(
-        arguments[1].into_int_value(),
-        arguments[2].into_int_value(),
-        "calldata_range_end_bytes",
-    );
-    let range_end = context.builder.build_int_unsigned_div(
-        range_end_bytes,
-        context.field_const(compiler_common::size::FIELD as u64),
-        "calldata_range_end",
-    );
-
-    let is_calldata_available = context.builder.build_int_compare(
-        inkwell::IntPredicate::UGE,
-        calldata_size,
-        range_end,
-        "calldata_is_available",
-    );
-    context.build_conditional_branch(is_calldata_available, copy_block, zero_block);
-
-    context.set_basic_block(copy_block);
     let destination = context.access_memory(
         arguments[0].into_int_value(),
         compiler_common::AddressSpace::Heap,
@@ -115,71 +80,9 @@ pub fn copy<'ctx, 'src>(
         destination,
         source,
         size,
-        "calldata_copy_memcpy_from_parent",
+        "calldata_copy_memcpy_from_child",
     );
-    context.build_unconditional_branch(join_block);
 
-    // TODO: remove if VM provides zeros after actual calldata
-    context.set_basic_block(zero_block);
-    let condition_block = context.append_basic_block("calldata_copy_zero_loop_condition");
-    let body_block = context.append_basic_block("calldata_copy_zero_loop_body");
-    let increment_block = context.append_basic_block("calldata_copy_zero_loop_increment");
-
-    let index_pointer = context.build_alloca(
-        context.field_type(),
-        "calldata_copy_zero_loop_index_pointer",
-    );
-    context.build_store(index_pointer, arguments[0]);
-    let range_end = context.builder.build_int_add(
-        arguments[0].into_int_value(),
-        arguments[2].into_int_value(),
-        "calldata_copy_zero_loop_range_end",
-    );
-    context.build_unconditional_branch(condition_block);
-
-    context.set_basic_block(condition_block);
-    let index_value = context
-        .build_load(
-            index_pointer,
-            "calldata_copy_zero_loop_index_value_condition",
-        )
-        .into_int_value();
-    let condition = context.builder.build_int_compare(
-        inkwell::IntPredicate::ULT,
-        index_value,
-        range_end,
-        "calldata_copy_zero_loop_condition",
-    );
-    context.build_conditional_branch(condition, body_block, join_block);
-
-    context.set_basic_block(increment_block);
-    let index_value = context
-        .build_load(
-            index_pointer,
-            "calldata_copy_zero_loop_index_value_increment",
-        )
-        .into_int_value();
-    let incremented = context.builder.build_int_add(
-        index_value,
-        context.field_const(compiler_common::size::FIELD as u64),
-        "calldata_copy_zero_loop_index_value_incremented",
-    );
-    context.build_store(index_pointer, incremented);
-    context.build_unconditional_branch(condition_block);
-
-    context.set_basic_block(body_block);
-    let index_value = context
-        .build_load(index_pointer, "calldata_copy_zero_loop_index_value_body")
-        .into_int_value();
-    let pointer = context.access_memory(
-        index_value,
-        compiler_common::AddressSpace::Heap,
-        "calldata_copy_zero_pointer_body",
-    );
-    context.build_store(pointer, context.field_const(0));
-    context.build_unconditional_branch(increment_block);
-
-    context.set_basic_block(join_block);
     None
 }
 
